66fd6480785e3798ab4569e99250d8e289d44b15
Md Halim Mondol <95875201+mhreza-ruet@users.noreply.github.com>
2025-11-19 11:30:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ smiles = st.text_input(
     placeholder="Paste a SMILES string…",
 )
 
-def centered_responsive_image(img, width_pct=80):
+def centered_responsive_image(img, width_pct=100):
     """
     Display a PIL image centered using a percentage width inside its container.
     """
